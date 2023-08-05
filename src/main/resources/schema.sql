45616a814ca5cbb3614b594852820299e254d0f7
Joan Janku <janku.joan4@gmail.com>
2023-08-05 02:38:42 +0200

    add db schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,28 @@
+create table author
+(
+    full_name varchar(17) null,
+    id        int auto_increment
+        primary key
+);
+
+create table book
+(
+    id          int auto_increment
+        primary key,
+    title       varchar(255) not null,
+    n_pages     int          not null,
+    price       int          not null,
+    copies_sold int          not null,
+    year        int          not null
+);
+
+create table author_book
+(
+    author_id int null,
+    book_id   int null,
+    constraint author_book_author_id_fk
+        foreign key (author_id) references author (id),
+    constraint author_book_book_id_fk
+        foreign key (book_id) references book (id)
+);
+
